a51e273261071843ab01e3c0c857a88502037611
endrber <endritpb@gmail.com>
2025-05-25 19:31:27 -0400

    add option for csr;sparse

--- examples/pegasos.py
+++ examples/pegasos.py
@@ -1,18 +1,15 @@
 # https://home.ttic.edu/~nati/Publications/PegasosMPB.pdf
 import numpy as np
 from tqdm import tqdm
+from extra.helpers import sparse
 from extra.datasets import fetch_amazon_polarity
-
-def hinge_loss(X, Y, theta, theta_0):
-  return np.maximum(0, 1-Y*(X@theta+theta_0)).mean()
 
 # NOTE: will strongly bias the selection towards particular kinds of classifiers
 # We want to find a new weight vector theta_{t+1} that is:
 #	•	Close to the old weights theta (don’t want big changes), and
 #	•	Correctly classifies the current example with margin at least 1
 def passive_aggressive(X, Y, T=10, C=1, variant="PA"):
-  # linear classifier that explicitly minimizes the hinge loss
-  # C = 1/lambda
+  # https://jmlr.csail.mit.edu/papers/volume7/crammer06a/crammer06a.pdf
   match variant:
     case "PA": fn = lambda l, norm_sq: l / norm_sq
     case "PA-I": fn = lambda l, norm_sq: min(C, l / norm_sq)
@@ -21,26 +18,31 @@ def passive_aggressive(X, Y, T=10, C=1, variant="PA"):
   theta, theta_0 = np.zeros(X.shape[1], np.float32), 0.0
   for t in range(T):
     for i in tqdm(np.random.permutation(X.shape[0]), desc=f"Epoch [{t+1}/{T}]"):
-      norm_sq = np.dot(X[i],X[i])
+      xi = sparse(X[i])
+      norm_sq = np.dot(xi, xi)
       if norm_sq == 0: continue  
-      eta = fn(hinge_loss(X[i],Y[i],theta,theta_0), norm_sq)
-      theta += eta*Y[i]*X[i]; theta_0 += eta*Y[i]
+      eta = fn(np.maximum(0, 1 - Y[i] * (xi @ theta + theta_0)).mean(), norm_sq) # lagrange multipler eta > 0
+      theta += eta*Y[i]*xi
+      theta_0 += eta*Y[i]
   return theta, theta_0
 
 # Learning a classifier as an optimization problem
-def pegasos(X, Y, lambda_, T=100, batch_size=32):
+def pegasos(X, Y, T=100, lambda_=1e-4, batch_size=32):
   theta, theta_0 = np.zeros(X.shape[1]), 0.0
-  for t in tqdm(range(1, T+1)):
+  for t in (pbar:=tqdm(range(1, T+1))):
+    pbar.set_description(f"Epoch [{t-1}/{T}]")
     indices = np.random.choice(X.shape[0], size=batch_size, replace=False)
     X_b, Y_b = X[indices], Y[indices]
-    A = np.where(Y_b*(X_b@theta+theta_0) < 1)[0]
+    A = np.where(Y_b * (X_b @ theta + theta_0) < 1)[0]
     eta = 1.0 / (lambda_ * t)
     if len(A) > 0:
-      grad_theta, grad_theta_0 = np.sum(Y_b[A, None]*X_b[A], axis=0), np.sum(Y_b[A])
+      grad_theta = X_b[A].multiply(Y_b[A][:, None]).sum(axis=0).A1 if hasattr(X_b[A], "multiply") else np.sum(Y_b[A, None] * X_b[A], axis=0)
+      grad_theta_0 = np.sum(Y_b[A])
     else:
-      grad_theta, grad_theta_0 = 0.0, 0.0
-    theta = (1-eta*lambda_)*theta + (eta/batch_size) * grad_theta
-    theta_0 = theta_0 + (eta/batch_size) * grad_theta_0  
+      grad_theta = np.zeros_like(theta)
+      grad_theta_0 = 0.0
+    theta = (1 - eta * lambda_) * theta + (eta / batch_size) * grad_theta
+    theta_0 = theta_0 + (eta / batch_size) * grad_theta_0
     w_norm = np.linalg.norm(theta)
     if w_norm != 0:
       theta = min(1.0, 1.0 / (np.sqrt(lambda_) * w_norm)) * theta
@@ -48,14 +50,14 @@ def pegasos(X, Y, lambda_, T=100, batch_size=32):
 
 if __name__ == "__main__":
   X_train, Y_train, X_test, Y_test = fetch_amazon_polarity()
-  Y_train, Y_test = np.where(Y_train==0,-1,1), np.where(Y_test==0,-1,1)
+  Y_train, Y_test = np.where(Y_train == 0, -1, 1), np.where(Y_test == 0, -1, 1)
   print(X_train.shape)
 
-  variant = "PA-I"
-  theta, theta_0 = passive_aggressive(X_train, Y_train, T=10, C=1, variant=variant)
-  print(f"[Passive-Aggressive {variant}] test_accuracy: {(np.sign(X_test@theta+theta_0)==Y_test).mean():.4f}\n")
-
-  theta, theta_0 = pegasos(X_train, Y_train, T=5000, lambda_=1e-5, batch_size=32)
-  print(f"[Mini-Batch Pegasos] test_accuracy: {(np.sign(X_test@theta+theta_0)==Y_test).mean():.4f}\n")
+  variant = "PA-II"
+  theta, theta_0 = passive_aggressive(X_train, Y_train, T=5, C=0.1, variant=variant)
+  print(f"[Passive-Aggressive {variant}] test_accuracy: {(np.sign(X_test @ theta + theta_0) == Y_test).mean():.4f}\n")
   
-   
+  batch_size = 256
+  theta, theta_0 = pegasos(X_train, Y_train, T=10*(X_train.shape[0]//batch_size), lambda_=5e-3, batch_size=batch_size)
+  print(f"[Mini-Batch Pegasos] test_accuracy: {(np.sign(X_test @ theta + theta_0) == Y_test).mean():.4f}\n")
+  
--- examples/perceptron.py
+++ examples/perceptron.py
@@ -1,6 +1,7 @@
 # https://cs.nyu.edu/~mohri/pub/pmb.pdf
-import os, numpy as np
+import numpy as np
 from tqdm import tqdm
+from extra.helpers import sparse
 from extra.datasets import fetch_amazon_polarity
 
 def perceptron(X, Y, T=10):
@@ -8,37 +9,35 @@ def perceptron(X, Y, T=10):
   for t in range(T):
     k = 0
     for i in tqdm(np.random.permutation(X.shape[0]), desc=f"Epoch [{t+1}/{T}]"):
-      if Y[i]*(X[i]@theta+theta_0) <= 1e-8:
-        theta += Y[i]*X[i]; theta_0 += Y[i]
+      xi = sparse(X[i])
+      if Y[i] * (xi @ theta + theta_0) <= 1e-8:
+        theta += Y[i] * xi
+        theta_0 += Y[i]
         k += 1
   return theta, theta_0, k
 
-# We can tweak the algorithm to extract a decent classifier using averaged perceptron
 def averaged_perceptron(X, Y, T=10):
   theta, theta_0 = np.zeros(X.shape[1], np.float32), 0.0       
   theta_bar, theta_0_bar, C = np.zeros_like(theta), 0.0, 1       
   stp = 1.0 / (X.shape[0] * T)
   for t in range(T):                             
     for i in tqdm(np.random.permutation(X.shape[0]), desc=f"Epoch [{t+1}/{T}]"):
-      if Y[i]*(theta@X[i]+theta_0) <= 1e-8:   
-        theta += Y[i]*X[i]; theta_0 += Y[i]                          
-        theta_bar += C*Y[i]*X[i]; theta_0_bar += C*Y[i]                  
+      xi = sparse(X[i])
+      if Y[i] * (theta @ xi + theta_0) <= 1e-8:   
+        theta += Y[i] * xi
+        theta_0 += Y[i]                          
+        theta_bar += C * Y[i] * xi
+        theta_0_bar += C * Y[i]                  
       C -= stp                      
   return theta_bar, theta_0_bar                  
 
 if __name__ == "__main__":
   X_train, Y_train, X_test, Y_test = fetch_amazon_polarity()
-  Y_train, Y_test = np.where(Y_train==0,-1,1), np.where(Y_test==0,-1,1)
+  Y_train, Y_test = np.where(Y_train == 0, -1, 1), np.where(Y_test == 0, -1, 1)
   print(X_train.shape)
   
-  theta, theta_0, mistakes = perceptron(X_train, Y_train, T=90)
-  if os.getenv("VERBOSE") != None:
-    print("\n[Perceptron] Convergence-Theorem:")
-    R = np.max(np.linalg.norm(X_train, axis=1)) # should be 1 since we normalize
-    margin = (X_train@theta+theta_0)
-    gamma = np.min((Y_train*margin)/np.linalg.norm(theta)) # Use the perceptron weights θ as an approximation of θ* 
-    print(f"k={mistakes} γ={gamma} train_accuracy: {(np.sign(margin)==Y_train).mean():.2f} - PMB (R/γ)^2: {(R / gamma)**2:.2f}")
-  print(f"[Realizable Case Perceptron] test_accuracy: {(np.sign(X_test@theta+theta_0)==Y_test).mean():.4f} \n")
+  theta, theta_0, mistakes = perceptron(X_train, Y_train, T=10)
+  print(f"[Realizable Case Perceptron] test_accuracy: {(np.sign(X_test @ theta + theta_0) == Y_test).mean():.4f} \n")
   
   theta_bar, theta_bar_0 = averaged_perceptron(X_train, Y_train, T=10)
-  print(f"[Averaged Perceptron] test_accuracy: {(np.sign(X_test@theta_bar+theta_bar_0)==Y_test).mean():.4f}")
+  print(f"[Averaged Perceptron] test_accuracy: {(np.sign(X_test @ theta_bar + theta_bar_0) == Y_test).mean():.4f}")
