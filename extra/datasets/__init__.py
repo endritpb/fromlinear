f8a62671c332538f3091eab892412af2677c3cc5
endrber <endritpb@gmail.com>
2025-06-04 08:26:21 -0400

    add tensor load

--- extra/datasets/__init__.py
+++ extra/datasets/__init__.py
@@ -9,7 +9,7 @@ def fetch_amazon_polarity():
   (X_train, Y_train), (X_test, Y_test) = parse(f"{BASE}/train-00000-of-00004.parquet"), parse(f"{BASE}/test-00000-of-00001.parquet")
   return vectorizer.fit_transform(X_train), Y_train, vectorizer.transform(X_test), Y_test
 
-def fetch_mnist():
+def fetch_mnist(tensor=False):
   # https://github.com/tinygrad/tinygrad/blob/master/extra/datasets/__init__.py#L6
   BASE_URL = "https://storage.googleapis.com/cvdf-datasets/mnist/" 
   parse = lambda file: np.frombuffer(gzip.open(fetch(file)).read(), dtype=np.uint8).copy()
@@ -17,4 +17,8 @@ def fetch_mnist():
   Y_train = parse(f"{BASE_URL}train-labels-idx1-ubyte.gz")[8:].astype(np.int8)
   X_test = parse(f"{BASE_URL}t10k-images-idx3-ubyte.gz")[0x10:].reshape((-1, 28*28)).astype(np.float32)
   Y_test = parse(f"{BASE_URL}t10k-labels-idx1-ubyte.gz")[8:].astype(np.int8)
+  if tensor:
+    import torch
+    tt = lambda x: torch.from_numpy(x)
+    return tt(X_train), tt(Y_train).long(), tt(X_test), tt(Y_test).long()
   return X_train, Y_train, X_test, Y_test
